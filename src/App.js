edccc1c6a1873018e110a31ff988749e34ce560e
asher-miti <asher_3@hotmail.co.uk>
2022-05-17 20:41:10 +0100

    new updated files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
@@ -6,7 +9,27 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+import { setCurrentUser } from './store/user/user.action';
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { compose, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
+
+import { rootReducer } from './root-reducer';
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
